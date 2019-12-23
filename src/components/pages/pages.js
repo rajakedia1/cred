a96e80b418e3bd24776e27c98784c4660adf6214
Raja Kumar Kedia <raja.kumarkedia@flipkart.com>
2019-12-23 16:15:43 +0530

    edited index.html

--- src/components/pages/pages.js
+++ src/components/pages/pages.js
@@ -5,13 +5,17 @@ import styles from "./pages.scss";
 
 function Pages({ match }) {
   const obj = {
-    search: {img: "1", back: '/product'},
-    browse: {img: "2", back: '/search'},
-    product: {img: "3", back: '/browse'}
+    search: { img: "1", back: "/product" },
+    browse: { img: "2", back: "/search" },
+    product: { img: "3", back: "/browse" }
   };
-	
-	const name = match.url.match(/[^/].+/)[0];
-	console.log("M: ", match, name, obj[name]);
+
+  const namelist = match.url.match(/[^/].+/);
+  if (!namelist) {
+    return <div>Home</div>;
+	}
+	const name = namelist[0];
+  console.log("M: ", match, name, obj[name]);
   return (
     <div className={styles["container"]}>
       <Screen screenID={obj[name]} />
